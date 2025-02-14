0b62d0f0de4115a9afc8d13e8354b37d8b57303a
CristianMartinezApi <fernandesribe04@gmail.com>
2025-02-13 23:31:12 -0300

    novo

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    if (!header) return;
+
+    let lastScrollY = window.scrollY;
+    let ticking = false;
+
+    function onScroll() {
+        if (window.scrollY > lastScrollY) {
+            header.classList.add('header-small');
+        } else {
+            header.classList.remove('header-small');
+        }
+        lastScrollY = window.scrollY;
+        ticking = false;
+    }
+
+    window.addEventListener('scroll', function() {
+        if (!ticking) {
+            window.requestAnimationFrame(onScroll);
+            ticking = true;
+        }
+    });
+});
