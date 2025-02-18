4a2b9b10e5f4bf75363aa52942fa5eab81475b71
CristianMartinezApi <fernandesribe04@gmail.com>
2025-02-18 15:01:43 -0300

    feita alterações em animação do header e inclusão de pdf na section

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('header-hidden');
+    } else {
+        header.classList.remove('header-hidden');
+    }
+});
